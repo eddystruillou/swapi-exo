f7137ab8d036cd172b202c24f18ca11acdd27a0f
eddystruillou <eddy.s25@hotmail.fr>
2019-04-25 21:11:45 +0200

    Force tu n'as pas ¤

--- index.js
+++ index.js
@@ -5,62 +5,45 @@ process.argv.forEach(function (val, index, array) {
 });
 
 const request = require("request");
-//let url = `https://swapi.co/api/films/${filmNumber}/`;
-//let data = '';
 
-function initialize() {
-    let url = `https://swapi.co/api/films/${filmNumber}/`;
-
+function getData(url) {
     return new Promise((resolve, reject) => {
         // Do async job
         request.get(url, (err, resp, body) => {
             if (err) {
                 reject(err);
             } else {
-                resolve(body); //JSON.parse possible crash ?
+                resolve(JSON.parse(body)); //JSON.parse possible crash ?
             }
         })
     })
 }
 
 function main() {
-    let initializePromise = initialize();
-    initializePromise.then((result) => {
-        let userDetails = result;
-        console.log(userDetails.planets)
-    }, (err) => {
-        console.log(err);
-    }).then(() => {
+    let url = `https://swapi.co/api/films/${filmNumber}/`;
+    let planets = [];
+    let planetsInfos = [];
+    let completed_requests = 0;
 
-    })
+    return new Promise(resolve => {
+        getData(url).then((result) => {
+            planets = result.planets;
+            url = planets[0];
+            for (let i in planets) {
+                getData(planets[i]).then((result) => {
+                    planetsInfos.push([result.name, result.diameter, result.terrain, result.surface_water]);
+                    completed_requests++;
+                    if (completed_requests === planets.length) {
+                        planetsInfos.forEach((element) => {
+                            if(element[3] === '1') {
+                                console.log(element);
+                            }
+                        });
+                    }
+                });
+            }
+        })
+    });
 }
 
 main();
-
-/*https.get(url, res => {
-    let planets = [];
-    let responses = [];
-    let completed_requests = 0;
-
-    res.on('data', chunk => {
-        data += chunk;
-    });
-
-    res.on('end', () => {
-        data = JSON.parse(data);
-        planets = data.planets;
-        console.log('data', data);
-
-        for (let i in planets) {
-            https.get(planets[i], res => {
-                responses.push(res);
-                completed_requests++;
-                if (completed_requests === planets.length) {
-                    // All download done, process responses array
-                    console.log(responses);
-                }
-                console.log(responses);
-            });
-        }
-    });
-});*/
