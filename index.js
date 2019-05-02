192783c45aa99127c71f27d8869bf701a38e2a94
eddystruillou <eddy.s25@hotmail.fr>
2019-05-02 17:20:12 +0200

    up

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+const request = require("request");
 let filmNumber = 1;
 
 process.argv.forEach(function (val, index, array) {
     filmNumber = val
 });
 
-const request = require("request");
-
 function getData(url) {
     return new Promise((resolve, reject) => {
-        // Do async job
         request.get(url, (err, resp, body) => {
             if (err) {
                 reject(err);
@@ -26,29 +24,30 @@ function main() {
     let mountains = "mountains";
     let completed_requests = 0;
     let diameterAvailable = [];
-    let diameterFinal;
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-    return new Promise(resolve => {
+    return new Promise(() => {
         getData(url).then((result) => {
             planets = result.planets;
             url = planets[0];
-            for (let i in planets) {
-                getData(planets[i]).then((result) => {
-                    planetsInfos.push([result.name, result.diameter, result.terrain, result.surface_water]);
-                    completed_requests++;
-                    if (completed_requests === planets.length) {
-                        planetsInfos.forEach((element) => {
-                            if(parseInt(element[3]) >= 1 && element[3] !== 'unknown') {
-                                if(element[2].toLowerCase().indexOf(mountains.toLowerCase()) !== -1) {
-                                    diameterAvailable = parseInt(element[1]);
-                                    console.log(diameterAvailable);
+            return new Promise(() => {
+                for (let i in planets) {
+                    getData(planets[i]).then((result) => {
+                        planetsInfos.push({name: result.name, diameter: result.diameter, terrain: result.terrain, sw: result.surface_water});
+                        completed_requests++;
+                        if (completed_requests === planets.length) {
+                            planetsInfos.forEach((element) => {
+                                if(element.sw >= 1 && element.sw !== 'unknown') {
+                                    if(element.terrain.toLowerCase().indexOf(mountains.toLowerCase()) !== -1) {
+                                        diameterAvailable = element.diameter;
+                                        console.log(diameterAvailable);
+                                    }
                                 }
-                            }
-                        });
-                    }
-                });
-            }
+                            });
+                        }
+                    });
+                }
+            })
         })
     });
 }
